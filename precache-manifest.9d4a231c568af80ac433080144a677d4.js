1d460b282d6b88efb433fadb23ed460b6a1a1666
Queen <queensform@Queens-MacBook-Air.local>
2020-05-18 12:21:27 -0700

    Updates

---
+++ precache-manifest.9d4a231c568af80ac433080144a677d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a7b664d52d137713f39837e10cdba7",
+    "url": "/seosift/index.html"
+  },
+  {
+    "revision": "cd21b5e4598ee8f4b659",
+    "url": "/seosift/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "c5282a3b3f081cd32559",
+    "url": "/seosift/static/css/main.0b00141f.chunk.css"
+  },
+  {
+    "revision": "cd21b5e4598ee8f4b659",
+    "url": "/seosift/static/js/2.412b39e6.chunk.js"
+  },
+  {
+    "revision": "88a28bf9811004a7250f8b652924c27d",
+    "url": "/seosift/static/js/2.412b39e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5282a3b3f081cd32559",
+    "url": "/seosift/static/js/main.e1fc2c4f.chunk.js"
+  },
+  {
+    "revision": "b2e253104717d98fd530",
+    "url": "/seosift/static/js/runtime-main.50a3e8f9.js"
+  }
+]);
